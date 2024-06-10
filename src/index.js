657103746d9b97215d77a355f5c48b2ac630358a
Martin <huy.nguyen12@sotatek.com>
2024-06-10 14:13:28 +0700

    change location

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import { NativeModules } from 'react-native';
 
 const { WifiManager } = NativeModules;
-const { RNWifiAndHotspotWizard } = NativeModules;
 
 export const CONNECT_ERRORS = {
     unavailableForOSVersion: 'unavailableForOSVersion',
@@ -47,37 +46,3 @@ export const LOAD_WIFI_LIST_ERRORS = {
 };
 
 export default WifiManager;
-
-class WifiWizard {
-    static turnOnWifi = () => RNWifiAndHotspotWizard.turnOnWifi();
-    static turnOffWifi = () => RNWifiAndHotspotWizard.turnOffWifi();
-    static isWifiEnabled = () => RNWifiAndHotspotWizard.isWifiEnabled();
-    static getNearbyNetworks = async () =>{
-       let devices =  await RNWifiAndHotspotWizard.startScan();
-       return JSON.parse(devices);
-    } 
-    static connectToNetwork = async (network,SSID,password) =>{
-       let message = await RNWifiAndHotspotWizard.connectToNetwork(JSON.stringify(network),SSID,password);
-       return JSON.parse(message);
-    } 
-    static disconnectFromNetwork = () => RNWifiAndHotspotWizard.disconnectFromNetwork();
-    static isReadyForCommunication = () => RNWifiAndHotspotWizard.isReadyForCommunication();
-}
-
-class HotspotWizard {
-    static turnOnHotspot = async (SSID,Password) =>{
-        WifiWizard.turnOffWifi();
-        let status = await  RNWifiAndHotspotWizard.turnOnHotspot(SSID,Password);
-        return JSON.parse(status);
-    } 
-    static isHotspotEnabled = () => RNWifiAndHotspotWizard.isHotspotEnabled()
-    static turnOffHotspot = async () =>{
-        let message = await RNWifiAndHotspotWizard.turnOffHotspot();
-        return JSON.parse(message);
-    } 
-}
-export {
-    WifiWizard,
-    HotspotWizard,
-    RNWifiAndHotspotWizard,
-}
